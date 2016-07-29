d3641d31f30295ff13ddf7ec3583969f38a1788c
Sahasha Reese <codeHer@icloud.com>
2016-07-29 00:56:34 -0500

    first commit

---
+++ public/assets/app.js
@@ -0,0 +1,43 @@
+angular.module('passApp', ['ngRoute']);
+
+angular.module('passApp').config(function($routeProvider, $locationProvider){
+  $routeProvider.when('/success', {
+    templateUrl: '/views/success.html'
+  }).when('/failure', {
+    templateUrl: '/views/failure.html'
+  }).when('/', {
+    templateUrl: '/views/login.html'
+  });
+
+  $locationProvider.html5Mode(true);
+});
+
+angular.module('passApp').controller('MainController', ['$http', '$location', function($http, $location){
+  var vm = this;
+  var data = {};
+
+  vm.login = function(){
+    console.log('attempt');
+    data.username = vm.username;
+    data.password = vm.password;
+    console.log(data);
+    $http.post('/login', data).then(function(response){
+      $location.path('/success');
+      console.log('Success');
+    }, function(err){
+      $location.path('/failure');
+      console.log(err);
+    });
+};
+
+vm.register = function(){
+  data.username = vm.username;
+  data.password = vm.password;
+  console.log(data);
+  $http.post('/register', data).then(function(response){
+    console.log('Great Success');
+  }, function(err){
+    console.log(err);
+  });
+};
+}]);
